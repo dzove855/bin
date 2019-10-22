ec8c671c106a2fbbc60536ae4a26e3bbeaf3e53d
dzove855 <dzove855@silvercry.ovh>
2019-10-22 17:04:48 +0200

    Create count.sh

---
+++ bin/count.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+SELF="${BASH_SOURCE[0]##*/}"
+NAME="${SELF%.sh}"
+
+OPTS="d:m:f:SrsvxEh"
+USAGE="Usage: $SELF [$OPTS] dir1 dir2 ..."
+
+HELP="
+$USAGE
+
+    Options:
+        -S      Super glob (**)
+        -r      Recursive 
+        -m      Max depth (with -r)
+        -d      exclude dir (not implented)
+        -f      exclude symlinks (not implemented)
+        -s      Simulate
+        -v      set -v
+        -x      set -x
+        -E      set -vE
+        -h      Help
+
+
+    NOTE: Do not set final / in dir name
+
+"
+
+function _quit()
+{
+    local retCode="$1" msg="${*:2}"
+
+    printf '%s\n' "$msg"
+    exit $retCode
+}
+
+function _count()
+{
+    local dir="$1" numfiles n _regex="$2"
+
+    shopt -s nullglob
+    shopt -s extglob
+    shopt -s globstar
+
+    numfiles=(${dir%/}/$_regex)
+    n="${#numfiles[@]}"
+    printf "%s : %s\n" "${dir%/}" "$n"
+}
+
+# default dir
+_dirs=("./")
+_regex="*"
+
+while getopts "${OPTS}" arg; do
+    case "${arg}" in
+        r) _find=1; opts+=" -type d"                                            ;;
+        m) _maxdepth=" -maxdepth ${OPTARG}"                                     ;;
+        d) _regex="${OPTARG}"                                                   ;;
+        f) _exclude_symlink=1                                                   ;;
+        S) _superGlob=1                                                         ;;
+        s) _run="echo"                                                          ;;
+        v) set -v                                                               ;;
+        x) set -x                                                               ;;
+        E) set -vE                                                              ;;
+        h) _quit 0 "$HELP"                                                      ;;
+        ?) _quit 1 "Invalid Argument: $USAGE"                                   ;;
+        *) _quit 1 "$USAGE"                                                     ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+[[ $# -eq 0 ]] || _dirs=($@)
+
+
+# Run Commands for each dir
+for _dir in "${_dirs[@]}"
+do
+    #Super glob
+    if [[ $_superGlob ]]
+    then
+        _regex="**"
+        _count "${_dir}" "$_regex"
+    # check if find is defined
+    elif [[ $_find ]]
+    then
+        # Run count on subdirs
+        for _sub_dir in $(find ${_dir} $_maxdepth $opts)
+        do
+            _count "$_sub_dir" "$_regex"
+        done
+    else
+        # run count on current dir
+        _count "${_dir}" "$_regex"
+    fi
+done
